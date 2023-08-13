662d67e738bc311fcde7f05fceeb913e37025c47
milenacurtinhas <116603940+milenacurtinhas@users.noreply.github.com>
2023-08-13 14:53:41 -0300

    Update main3.c

--- main3.c
+++ main3.c
@@ -1,20 +1,17 @@
 #include "libraries.h"
 
-
 int main(int argc, char *argv[]){
     FILE* readDocuments;
+    FILE* binary;
     char directory1[1000];
     sprintf(directory1,"%s", argv[2]);
     
-    readDocuments = fopen(directory1, "r");
-
     if(argc != 5){
-        printf ("ERRO:Os arquivos de dados devem estar contidos na pasta 'datasets' e o programa devera ser executado como o exemplo:\n");
-        printf ("./prog3 binary.bin datasets/\"nomedapasta\"/test.txt \"numero K\" \"nomedoarquivodesaida\"/\n");
+        printf ("ERRO: O programa deve ser executado como o exemplo:\n");
+        printf ("./prog3 binary.bin \"CaminhoParaPasta\"/test.txt \"numero K\" \"NomeArquivoDeSaida\".txt\n");
         exit(1);
     }
-
-    FILE* binary;
+    readDocuments = fopen(directory1, "r");
     binary = fopen(argv[1], "rb");
     
     if(binary == NULL){
@@ -22,23 +19,31 @@ int main(int argc, char *argv[]){
         exit(1);
     }
 
+    if(readDocuments == NULL){
+        printf ("ERRO:Não foi possível abrir o arquivo .txt, o programa deve ser executado como o exemplo:\n");
+        printf ("./prog3 binary.bin \"CaminhoParaPasta\"/test.txt \"numero K\" \"NomeArquivoDeSaida\".txt\n");
+        exit(1);
+    }
+    
     tNewsLetter* TrainNewsletter;
     TrainNewsletter = ReadBinary(binary);
-
-    tNewsLetter *TestNewsletter = InitializeNewsletter();
+    
+    tNewsLetter *TestNewsletter = InitializeTestNewsletter();
     TestNewsletter = GetNewsTitle(readDocuments, TestNewsletter);
+    TestNewsletter = InitializeNewsClass(TestNewsletter);
+    
     int k;
     k = atoi(argv[3]);
     TestNewsletter = GetTestClass(TrainNewsletter, TestNewsletter, argv[2], k);
-
+    
     int** ConfusionMatrix = calloc(ClassesQtt, sizeof(int*));
-
     CalculateMatrix(ConfusionMatrix, TestNewsletter, argv[4]);
-
+    
     FreeNewsLetter(TrainNewsletter);
-    FreeNewsLetter(TestNewsletter);
+    FreeTestNewsLetter(TestNewsletter);
+    
     fclose(readDocuments);
     fclose(binary);
-
+    
     return 0;
 }
