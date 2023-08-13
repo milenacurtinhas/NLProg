9cafdeceb01419e8db34ac09baeb1df5178cda89
milenacurtinhas <116603940+milenacurtinhas@users.noreply.github.com>
2023-08-13 15:01:17 -0300

    Create  libraries.h

---
+++ include/libraries.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+
+#include "tNewsLetter.h"
+#include "tDocument.h"
+#include "tWord.h"
+#include "tClass.h"
+
+#define ClassesQtt 11
